fe922795e1aa2b9736c9efb3540a9104fd5b3e03
DEVANAND GIRI <g.dev9716@gmail.com>
2022-08-19 23:45:02 +0530

    scroll

---
+++ index.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container');
+
+let limit = 4;
+let pageCount = 1;
+let postCount=1;
+
+// Steps:
+/* 
+pt.1
+    get a batch of images and append to the container div
+*/
+const getPost = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/comments?_limit=${limit}&_page=${pageCount}`);
+    //console.log(response);
+    const data = await response.json();
+    //console.log(data[0].body);
+    data.map((curElm,index) => {
+        const htmlData  = `<div class="posts">
+        <p class="post-id">${postCount++}</p>
+        <h2 class="title">${curElm.title}</h2>
+        <p class="post-info"> ${curElm.body}</p>
+    </div>`;
+
+    container.insertAdjacentHTML('beforeend',htmlData)
+    });
+}
+getPost();
+
+const showData = () =>{
+    setTimeout(() => {
+        pageCount++;
+        getPost();
+    }, 10)
+};
+/* 
+pt.2
+    listen for a scroll event and load more images if we reach the buttom of window
+*/
+window.addEventListener('scroll', () => {
+    const {scrollHeight, scrollTop, clientHeight} = document.documentElement;
+
+    if(scrollTop + clientHeight >= scrollHeight){
+        //console.log("I am here");
+        showData();
+    }
+});
