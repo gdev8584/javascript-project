3b5597198160e252f06cd0eb51b94e4c41cf651c
DEVANAND GIRI <g.dev9716@gmail.com>
2022-10-06 19:43:45 +0530

    music-making

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const video = document.getElementById("myVideo")
+
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      video.currentTime += 10;
+      video.play();
+    });
+  });
+});
